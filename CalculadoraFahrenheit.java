62c157e40efb2c618e7b23982555900162656ee5
Matheus Souza Komo <matheus.sbs03@gmail.com>
2025-04-24 14:02:04 -0300

    CalculadoraFahrenheit 1.0

--- CalculadoraFahrenheit.java
+++ CalculadoraFahrenheit.java
@@ -3,22 +3,35 @@ import java.util.Scanner;
 public class CalculadoraFahrenheit {
     public static void main(String[] args) {
         //Declaração de Variáveis
-        double cel, fah; //cel - temperatura em graus celsius; fah - temperatura em graus fahrenheit;
+        double cel, fah, option; //cel - temperatura em graus celsius; fah - temperatura em graus fahrenheit;
 
         //Criação e instância do objet0 de entrada
         Scanner entrada = new Scanner(System.in);
         
         //Apresentação
         System.out.println("\n\t\t\t -- Conversor --\n");
+        System.out.println("1. Celsius para Fahrenheit.");
+        System.out.println("2. Fahrenheit para Celsius");
 
-        //Entradas
+        System.out.println("Opção: ");
+        option = entrada.nextInt();
+
+        //Entradas de Celsius para Fahrenheit.
+        if (option == 1){
         System.out.print("Informe a temperatura em °C: ");
         cel = entrada.nextDouble();
-
-        //Processamento
+        
+        //Processamento Celsius para Fahrenheit
         fah = cel * 9 / 5 + 32;
 
-        //Saída
+        //Saída Celsius para Fahrenheit
         System.out.println("\n" + cel + "°C = " + fah + "°F\n");
+        }
+
+        //Entradas de Fahrenheit para Celsius
+
+        //Processamento Fahrenheit para Celsius
+
+        //Saída Fahrenheit para Celsius
     }
 }
